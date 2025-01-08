179b93c415beb0daa16db865255f18ac1ff860da
alexhulbert <alex@alexhulbert.com>
2025-01-07 20:56:58 -0500

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    kernel = import ./kernel.nix { inherit pkgs; };
+    mkosi = pkgs.mkosi;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        apt dpkg gnupg debootstrap
+        squashfsTools dosfstools e2fsprogs mtools
+        cryptsetup util-linux zstd qemu
+        libseccomp
+      ] ++ [ mkosi ];
+
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.libseccomp ];
+      KERNEL_IMAGE = "${kernel}/bzImage";
+      KERNEL_VERSION = kernel.version;
+    };
+  };
+}
---
+++ kernel.nix
@@ -0,0 +1,55 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation rec {
+  pname = "linux-tdx";
+  version = "6.1.123";
+
+  depsBuildBuild = with pkgs.pkgsBuildBuild; [
+    stdenv.cc
+  ];
+
+  nativeBuildInputs = with pkgs.buildPackages; [
+    git flex bison elfutils openssl
+    bc perl gawk zstd
+  ];
+
+  src = pkgs.fetchFromGitHub {
+    owner = "gregkh";
+    repo = "linux";
+    rev = "v${version}";
+    sha256 = "sha256-WLkLJEICBEXCMPRadMFsYWweXz6E5Kk323OgJz5Gizg=";
+  };
+
+  # patches = [];
+
+  configurePhase = ''cp ${./kernel-yocto.config} .config'';
+
+  buildPhase = ''
+    patchShebangs ./scripts/ld-version.sh
+    export KBUILD_BUILD_TIMESTAMP="$(date -u -d @$SOURCE_DATE_EPOCH)"
+    export KBUILD_BUILD_USER="nixbuild"
+    export KBUILD_BUILD_HOST="nixbuilder"
+    make olddefconfig bzImage -j "$NIX_BUILD_CORES" \
+      ARCH="x86_64" \
+      HOSTCC="$CC_FOR_BUILD" \
+      HOSTCXX="$CXX_FOR_BUILD" \
+      HOSTAR="$AR_FOR_BUILD" \
+      HOSTLD="$LD_FOR_BUILD" \
+      CC="$CC" LD="$LD" \
+      OBJCOPY="$OBJCOPY" \
+      OBJDUMP="$OBJDUMP" \
+      READELF="$READELF" \
+      STRIP="$STRIP" \
+      CONFIG_EFI_STUB=y
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp arch/x86_64/boot/bzImage $out/
+  '';
+
+  meta = {
+    description = "Linux Kernel ${version}";
+    homepage = https://kernel.org;
+    license = "gpl2Only";
+  };
+}
